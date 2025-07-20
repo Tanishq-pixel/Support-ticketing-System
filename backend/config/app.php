9dd31da1cf5421ab41ca74b9d3e45e315f411812
Tanishq-pixel <tanishqkommuri@gmail.com>
2025-07-20 16:50:16 +0530

    Initial commit with composer.lock

---
+++ backend/config/app.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'Laravel'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => 'UTC',
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'key' => env('APP_KEY'),
+    'cipher' => 'AES-256-CBC',
+    'providers' => [
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        // ...existing code...
+    ],
+    'aliases' => [
+        // ...existing code...
+    ],
+];
